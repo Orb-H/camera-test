d794b8c4fb28323b6a4225f74be1c7c6cabe135f
Jongryeol Han <orb.cov@gmail.com>
2020-10-03 11:02:21 +0000

    add quaternion class

--- math_util.py
+++ math_util.py
@@ -210,3 +210,70 @@ class Vector:
         m = self.magnitude()
         return Vector(self.v[i] / m for i in range(len(self.v)))
 
+
+class Quaternion:
+    '''
+    A quaternion class.
+    '''
+
+    def __init__(self, v: Vector = None):
+        if v == None or len(v) == 0:  # Default Quaternion
+            self.q = [1, 0, 0, 0]
+        if len(v) == 3:  # Euler Angle (Yaw, Pitch, Roll)
+            self.q = Quaternion([math.cos(v[2] / 2), 0, 0, math.sin(v[2] / 2)]).composite(Quaternion([math.cos(
+                v[1] / 2), math.sin(v[1] / 2), 0, 0])).composite(Quaternion([math.cos(v[0] / 2), 0, math.sin(v[0] / 2)]))
+        elif len(v) == 4:  # Quaternion Value
+            m = v.magnitude()
+            self.q = v / m
+
+    def rotate(self, v: Vector):
+        '''
+        Rotates a vector by rotation defined by this.
+
+        PARAMETER
+            v: A vector to rotate.
+
+        RETURNS
+            A rotated vector.
+        '''
+        if len(v) != 3:
+            raise Exception("Given vector is not a 3d vector.")
+        ii = self.q[1] ** 2
+        jj = self.q[2] ** 2
+        kk = self.q[3] ** 2
+        ij = self.q[1] * self.q[2]
+        jk = self.q[2] * self.q[3]
+        ik = self.q[1] * self.q[3]
+        ir = self.q[0] * self.q[1]
+        jr = self.q[0] * self.q[2]
+        kr = self.q[0] * self.q[3]
+
+        return Vector([
+            (1 - 2 * (jj + kk)) * v[0] + 2 *
+            (ij - kr) * v[1], 2 * (ik + jr) * v[2],
+            2 * (ij + kr) * v[0] + (1 - 2 * (ii + kk)) *
+            v[1] + 2 * (jk - ir) * v[2],
+            2 * (ik - jr) * v[0] + 2 * (jk + ir) *
+            v[1] + (1 - 2*(ii + jj)) * v[2]
+        ])
+
+    def composite(self, other):
+        '''
+        Composites two rotations.
+
+        PARAMETER
+            other: A quaternion representing another rotation to composite with rotation defined by this.
+
+        RETURNS
+            composited two rotations.
+        '''
+        return Quaternion([
+            self.q[0] * other.q[0] - self.q[1] * other.q[1] -
+            self.q[2] * other.q[2] - self.q[3] * other.q[3],
+            self.q[0] * other.q[1] + self.q[1] * other.q[0] +
+            self.q[2] * other.q[3] - self.q[3] * other.q[2],
+            self.q[0] * other.q[2] + self.q[2] * other.q[0] +
+            self.q[3] * other.q[1] - self.q[1] * other.q[3],
+            self.q[0] * other.q[3] + self.q[3] * other.q[0] +
+            self.q[1] * other.q[2] - self.q[2] * other.q[1]
+        ])

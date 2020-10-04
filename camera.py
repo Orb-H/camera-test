204d6f42ed600275b037cbbf14b15558d4c55135
Jongryeol Han <orb.cov@gmail.com>
2020-10-04 07:59:15 +0000

    add physical info of camera

--- camera.py
+++ camera.py
@@ -1,4 +1,6 @@
+import math
 import tkinter as tk
+from math_util import *
 
 
 class Camera:
@@ -7,6 +9,9 @@ class Camera:
     '''
 
     def __init__(self, canvas, renderer=None):
+        self.pos = Vector([0, 0, 0])
+        self.rot = Quaternion()
+
         self.c = canvas
         self.r = renderer
 
@@ -14,3 +19,52 @@ class Camera:
         if self.c is None or self.r is None:
             return
         self.r.render()
+
+    # Position
+
+    def move(self, amount):
+        self.pos += amount
+
+    def move_forward(self, amount):
+        self.move(self.rot * Vector([0, 0, -amount]))
+
+    def move_backward(self, amount):
+        self.move_forward(-amount)
+
+    def move_right(self, amount):
+        self.move(self.rot * Vector([amount, 0, 0]))
+
+    def move_left(self, amount):
+        self.move_right(-amount)
+
+    def move_up(self, amount):
+        self.move(self.rot * Vector([0, amount, 0]))
+
+    def move_down(self, amount):
+        self.move_up(-amount)
+
+    # Rotation
+
+    def rotate(self, amount):
+        self.rot = amount * self.rot
+
+    def rotate_up(self, amount):
+        self.rotate(Quaternion(
+            Vector([math.cos(amount), math.sin(amount), 0, 0])))
+
+    def rotate_down(self, amount):
+        self.rotate_up(-amount)
+
+    def rotate_left(self, amount):
+        self.rotate(Quaternion(
+            Vector([math.cos(amount), 0, math.sin(amount), 0])))
+
+    def rotate_right(self, amount):
+        self.rotate_left(-amount)
+
+    def rotate_ccw(self, amount):
+        self.rotate(Quaternion(
+            Vector([math.cos(amount), 0, 0, -math.sin(amount)])))
+
+    def rotate_cw(self, amount):
+        self.rotate_ccw(-amount)
